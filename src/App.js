32d470ea053d30f1aaa953232a4a9dd0da4eb0e7
Evan Oyster <oyster.evan@gmail.com>
2022-12-11 10:42:13 -0500

    background image

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Header from './Components/Header'
 
 function App() {
   return (
     <div className="App">
-      <h1>sup fool</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Header() {
+    return (
+        <>
+            <label>Search Your Favorite Character</label>
+            <input type="text" />
+            <button>Run Search</button>
+        </>
+    )
+}
+
+export default Header;
+
