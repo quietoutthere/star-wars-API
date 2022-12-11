ae310e9709b866ad68353501585bec021454d947
Evan Oyster <oyster.evan@gmail.com>
2022-12-11 12:44:07 -0500

    search state update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { useState } from 'react';
 
 function Header() {
+    const [search, setSearch] = useState('')
+
+    const handleChange = (event) => {
+        setSearch(event.target.value)
+        console.log(search)
+    };
+
+    const handleClick = () => {
+
+    }
+
     return (
         <div className='header-text'>
             <h1>Search Your Favorite Character</h1>
-            <input type="text" placeholder='Search by Name, Birthdate, Height, Mass, Homeworld, Species'/>
-            <button>Run Search</button>
+            <input 
+            value={search}
+            type="text" 
+            id='input'
+            placeholder='Search by Name, Birthdate, Height, Mass, Homeworld, Species'
+            onChange={handleChange}
+            required
+            />
+            <button onClick={handleClick}>Run Search</button>
         </div>
     )
 }
