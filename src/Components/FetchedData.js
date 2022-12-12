df15a41f3eee1ff2835ca5f7fc82ed65c16c3494
Evan Oyster <oyster.evan@gmail.com>
2022-12-12 11:43:16 -0500

    bootstrap install error

--- src/Components/FetchedData.js
+++ src/Components/FetchedData.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Table from 'react-bootstrap/Table';
 
 function componentDidMount() {
     axios.get('https://swapi.dev/api/')
@@ -8,6 +9,40 @@ function componentDidMount() {
     .catch(error => {
       console.log(error);
     });
+
+    return (
+        <Table striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Birthdate</th>
+          <th>Height</th>
+          <th>Mass</th>
+          <th>Homeworld</th>
+          <th>Species</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+    )
   }
 
 export default componentDidMount;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -15,21 +15,23 @@ function Header() {
     }
 
     return (
-        <div className='header-text'>
-            <h1>Search Your Favorite Character</h1>
-            <input 
-            value={search}
-            type="text" 
-            id='input'
-            placeholder='Search by Name, Birthdate, Height, Mass, Homeworld, Species'
-            onChange={handleChange}
-            required
-            />
-            <button onClick={handleClick}>Run Search</button>
+        <>
+            <div className='header-text'>
+                <h1>Search Your Favorite Character</h1>
+                <input 
+                value={search}
+                type="text" 
+                id='input'
+                placeholder='Search by Name, Birthdate, Height, Mass, Homeworld, Species'
+                onChange={handleChange}
+                required
+                />
+                <button onClick={handleClick}>Run Search</button>
+            </div>
             <div>
                 <Table />
             </div>
-        </div>
+        </>
     )
 }
 
