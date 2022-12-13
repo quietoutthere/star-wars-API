9e88fb588d33dc632cb7e85abd38ab716617fe3b
Evan Oyster <oyster.evan@gmail.com>
2022-12-13 13:41:18 -0500

    map through index

--- src/Components/FetchedData.js
+++ src/Components/FetchedData.js
@@ -1,32 +1,41 @@
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Table from 'react-bootstrap/Table';
 
-function componentDidMount() {
-    axios.get('https://swapi.dev/api/')
-    .then(response => {
-      console.log(response.data);
-    })
-    .catch(error => {
-      console.log(error);
-    });
+function ComponentDidMount() {
 
+  const [index, setIndex] = useState()
+
+    useEffect(() => {
+      axios.get('https://swapi.dev/api/')
+      .then(response => {
+        console.log('Getting from :', response.data);
+        setIndex(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  
+    }, [])
+
+  
     return (
         <Table striped bordered hover variant="dark">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Birthdate</th>
-          <th>Height</th>
-          <th>Mass</th>
-          <th>Homeworld</th>
-          <th>Species</th>
-        </tr>
-      </thead>
-      <tbody>
-        
-      </tbody>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Birthdate</th>
+              <th>Height</th>
+              <th>Mass</th>
+              <th>Homeworld</th>
+              <th>Species</th>
+            </tr>
+          </thead>
+          <tbody>
+            
+          </tbody>
     </Table>
     )
   }
 
-export default componentDidMount;
+export default ComponentDidMount;
