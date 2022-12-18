20b0dc5c73e16a5e63a4857cffd5e17749bbe97d
Evan Oyster <oyster.evan@gmail.com>
2022-12-18 12:48:38 -0500

    pull from planets

--- src/Components/FetchedData.js
+++ src/Components/FetchedData.js
@@ -5,6 +5,7 @@ import Table from 'react-bootstrap/Table';
 function FetchedData({ search }) {
 
   const [characters, setCharacters] = useState([]);
+  const [homeworld, setHomeworld] = useState([]);
 
     useEffect(() => {
       Axios.get('https://swapi.dev/api/people/')
@@ -15,13 +16,25 @@ function FetchedData({ search }) {
       .catch(error => {
         console.log(error);
       });
+      Axios.get('https://swapi.dev/api/planets/1/')
+      .then(response => {
+        console.log('Getting from :', response.data.results);
+        setHomeworld(response.data.results);
+      })
+      .catch(error => {
+        console.log(error);
+      });
     }, [])
     
     const tableBody = characters.map((character) => {
-      debugger;
       return (
         <tr id={Math.random()} key='id'>
           <td>{ character.name }</td>
+          <td>{ character.birth_year }</td>
+          <td>{ character.height }</td>
+          <td>{ character.mass }</td>
+          <td>{ homeworld }</td>
+         
         </tr>
       )
     });
