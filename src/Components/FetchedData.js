f973a8d02c350e279320322130050ecbe423dd18
Evan Oyster <oyster.evan@gmail.com>
2022-12-28 09:50:55 -0500

    update

--- src/Components/FetchedData.js
+++ src/Components/FetchedData.js
@@ -5,7 +5,7 @@ import Table from 'react-bootstrap/Table';
 function FetchedData({ search }) {
 
   const [characters, setCharacters] = useState([]);
-  const [homeworld, setHomeworld] = useState([]);
+  const [homeWorlds, setHomeWorlds] = useState([]);
 
     useEffect(() => {
       Axios.get('https://swapi.dev/api/people/')
@@ -16,25 +16,29 @@ function FetchedData({ search }) {
       .catch(error => {
         console.log(error);
       });
-      Axios.get('https://swapi.dev/api/planets/1/')
+
+      Axios.get('https://swapi.dev/api/planets/')
       .then(response => {
         console.log('Getting from :', response.data.results);
-        setHomeworld(response.data.results);
+        setHomeWorlds(response.data.results);
       })
       .catch(error => {
         console.log(error);
       });
     }, [])
+
+    const worlds = homeWorlds.map((homeWorld) => {
+      return homeWorld
+    });
     
     const tableBody = characters.map((character) => {
       return (
-        <tr id={Math.random()} key='id'>
+        <tr id= {Math.random()} key='id'>
           <td>{ character.name }</td>
           <td>{ character.birth_year }</td>
           <td>{ character.height }</td>
           <td>{ character.mass }</td>
-          <td>{ homeworld }</td>
-         
+          <td>{ worlds.name }</td>
         </tr>
       )
     });
