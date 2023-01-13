31d92df48cc7315a43ee397234186ef974f6f25f
Evan Oyster <oyster.evan@gmail.com>
2023-01-13 14:43:36 -0500

    loading spinner added

--- src/Components/FetchedData.js
+++ src/Components/FetchedData.js
@@ -2,11 +2,15 @@ import { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import axios from 'axios';
 import React from 'react';
+import BootstrapTable from 'react-bootstrap-table2-paginator';
+import PaginationFactory from 'react-bootstrap-table2-paginator';
+import * as ReactBootstrap from 'react-bootstrap';
 
 function FetchedData({ search }) {
 
   const [characters, setCharacters] = useState([]);
-  const [url, setUrl] = useState('https://swapi.dev/api/people/')
+  const [loading, setLoading] = useState(false);
+  let url ='https://swapi.dev/api/people/';
 
   const getCharacterData = async (homeworldUrl) => {
     const { data } = await axios.get(homeworldUrl)
@@ -23,16 +27,15 @@ function FetchedData({ search }) {
           char.speciesName = await getCharacterData(char.species)
             if (!char.speciesName ) {
               char.speciesName = "Human"}
-
-          console.log(chars)
         }
         setCharacters(chars);
+        setLoading(true);
         
       }
       getData();
     }, [])
 
-    
+
     const tableBody = characters.map((character) => {
       return (
         <tr>
@@ -47,41 +50,28 @@ function FetchedData({ search }) {
     });
     
     return (
-        <Table className='table'>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Birthdate</th>
-              <th>Height</th>
-              <th>Mass</th>
-              <th>Homeworld</th>
-              <th>Species</th>
-            </tr>
-          </thead>
-          <tbody>
-             { tableBody }
-          </tbody>
-    </Table>
-    )
-  }
+      <>
+          <Table className='table'>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Birthdate</th>
+                <th>Height</th>
+                <th>Mass</th>
+                <th>Homeworld</th>
+                <th>Species</th>
+              </tr>
+            </thead>
+            <tbody>
+              {loading ? (
+              { tableBody }
+              ) : (
+                <ReactBootstrap.Spinner animation='border' className="spinner"/>
+              )}
+            </tbody>
+          </Table>
+      </>
+
+  )}
 
 export default FetchedData;
-
-
-// Axios.get(url)
-//       .then(response => {
-//         console.log('Getting from :', response.data.results);
-//         setCharacters(response.data.results);
-//       })
-//       .catch(error => {
-//         console.log(error);
-//       });
-
-//       Axios.get('https://swapi.dev/api/planets/')
-//       .then(response => {
-//         console.log('Getting from :', response.data.results);
-//         setHomeWorlds(response.data.results);
-//       })
-//       .catch(error => {
-//         console.log(error);
-//       });
