c95f0b499f882d63809f75d30b568a77a1c89d26
Evan Oyster <oyster.evan@gmail.com>
2023-01-24 17:50:00 -0500

    bootstrap css deleted

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
